df59b75b3645968a79a6fc03e83d522d3d46c9ea
babai-abhishek <babbi.abhishek@gmail.com>
2022-07-19 00:55:09 +0530

    file updated

--- Customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
+++ Customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class CustomerServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
